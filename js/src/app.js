e81788d755889dc6ecb2d1a740de46f7a0f94177
ryanbmarx <ryanbmarx@gmail.com>
2016-04-03 00:01:05 -0500

    Added more notes to myself. No changes in functionality whatsoever. A quick tweak, if you will.

--- js/src/app.js
+++ js/src/app.js
@@ -1,9 +1,11 @@
 /*
 
-##TO DO
+## TODO
 - Consider whether the dots should scale with the dead fish
 - Decide what data needs to be in the profile.
-- We're gonna need some geolocating help 
+- We're gonna need some geolocating help, but still don't have real data.
+- Programatically trigger a click so the buttons sync with the initial map data.
+- Figure out how to change color state of dots on click.
 
 */
 
