574779e76907b6779638497aa0954b258cc099ef
ryanbmarx <ryanbmarx@gmail.com>
2016-04-02 19:33:11 -0500

    Until I figure a better way to deliver this data (jsonify filter?), I'll use the built-in tarbell function that publishes the app's JSON. It's important to leave the profiles data in the spreadsheet because it needs to remain VERY editable.

--- tarbell_config.py
+++ tarbell_config.py
@@ -14,7 +14,7 @@ EXCLUDES = ['*.md', 'requirements.txt', 'node_modules', 'sass', 'js/src', 'packa
 # SPREADSHEET_CACHE_TTL = 4
 
 # Create JSON data at ./data.json, disabled by default
-# CREATE_JSON = True
+CREATE_JSON = True
 
 # Get context from a local file or URL. This file can be a CSV or Excel
 # spreadsheet file. Relative, absolute, and remote (http/https) paths can be 
