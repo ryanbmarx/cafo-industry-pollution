6aa936b3c5a875b8eb8ce038c25a05cee2ad04a0
ryanbmarx <ryanbmarx@gmail.com>
2016-05-06 14:34:34 -0500

    Remove console.logs, unneeded comments

--- js/src/cafo-profile-buttons.js
+++ js/src/cafo-profile-buttons.js
@@ -1,5 +1,5 @@
 var CafoProfileButtons = function(options){
-	console.log(options);
+
 	// Bind click handlers to button elements
 	var forEach = Array.prototype.forEach;
 	forEach.call(options.buttons, button => {
@@ -7,10 +7,8 @@ var CafoProfileButtons = function(options){
 			e.preventDefault();
 			// update choropleth map base on selected button/data- attribute
 			if(this.dataset.direction == "next"){
-				console.log('next');
 				options.map.showNextPollutionEvent();
 			} else {
-				console.log('back');
 				options.map.showPreviousPollutionEvent();
 			}
 		}, false);
